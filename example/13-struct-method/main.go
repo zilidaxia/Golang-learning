a1c412e22582b67571000e10c9265b1ab7895d39
zilidaxia <2214736768@qq.com>
2023-01-15 22:10:07 +0800

    Day01 基本语法

---
+++ example/13-struct-method/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// 结构体是可变的
+type user struct {
+	name     string
+	password string
+}
+
+func (u user) checkPassword(password string) bool {
+	return u.password == password
+}
+
+func (u *user) resetPassword(password string) {
+	u.password = password
+}
+
+func main() {
+	a := user{name: "wang", password: "1024"}
+	a.resetPassword("2048")
+	fmt.Println(a.checkPassword("2048")) // true
+}
